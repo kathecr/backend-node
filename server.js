ebc08ca08ec9bcd4705e541de45c78fe5d9fad99
kathecr <kcastelblancor@gmail.com>
2021-04-27 18:27:19 -0500

    Readbody and query of request

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 const express = require("express");
 // //Importar express con sintaxis de ECMAScript6
 // // import express from 'express'
-
+const bodyParser = require("body-parser");
 var app = express();
 const port = 3000;
 
-// app.use("/", (req, res) => res.send("Hola"));
-
-// app.get("/", (req, res) => res.send("Hola desde get"));
-// app.post("/", (req, res) => res.send("Hola desde post"));
-// app.put("/", (req, res) => res.send("Hola desde put"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/message", (req, res) => res.send("Lista de mensajes"));
-app.post("/message", (req, res) => res.send("Mensaje añadido"));
-app.delete("/message", (req, res) => res.send("Mensaje eliminado"));
-
-
-
-
-
+app.post("/message", (req, res) => res.send("Mensaje añadido correctamente"));
+app.delete("/message", (req, res) => {
+  console.log(req.query);
+  console.log(req.body);
+  res.send("Mensaje " + req.body.text + " eliminado");
+});
 
 app.listen(port, () =>
   console.log("La aplicación est escuchando en http://localhost:3000")
 );
-
