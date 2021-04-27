1a5cee17ea7e0e5322c277db5b03b80c106051a5
kathecr <kcastelblancor@gmail.com>
2021-04-27 17:24:46 -0500

    Create res to methods

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express = require('express')
-//Importar express con sintaxis de ECMAScript6
-// import express from 'express'
+const express = require("express");
+// //Importar express con sintaxis de ECMAScript6
+// // import express from 'express'
 
 var app = express();
-app.use('/', function(req,res){
-  res.send('Hola')
-});
+const port = 3000;
 
-app.listen(3000)
-console.log('La aplicación est escuchando en http://localhost:3000');
+// app.use("/", (req, res) => res.send("Hola"));
+
+// app.get("/", (req, res) => res.send("Hola desde get"));
+// app.post("/", (req, res) => res.send("Hola desde post"));
+// app.put("/", (req, res) => res.send("Hola desde put"));
+
+app.get("/message", (req, res) => res.send("Lista de mensajes"));
+app.post("/message", (req, res) => res.send("Mensaje añadido"));
+app.delete("/message", (req, res) => res.send("Mensaje eliminado"));
+
+
+
+
+
+
+app.listen(port, () =>
+  console.log("La aplicación est escuchando en http://localhost:3000")
+);
+
