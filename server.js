1834f5935d350a6b35448ba262d9b54743951c2d
kathecr <kcastelblancor@gmail.com>
2021-04-27 19:38:23 -0500

    Create function to send success or error in response

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
-const express = require("express");
 // //Importar express con sintaxis de ECMAScript6
 // // import express from 'express'
-const bodyParser = require("body-parser");
+const express = require("express");
+const response = require("./network/response");
+
 var app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/message", (req, res) => res.send("Lista de mensajes"));
-app.post("/message", (req, res) => res.send("Mensaje añadido correctamente"));
+app.get("/message", (req, res) => {
+  console.log(req.headers);
+  res.header({
+    "custom-header": "Nuestro valor personalizado",
+  });
+  // res.send("Lista de mensajes");
+  response.success(req, res, "Lista de mensajes");
+});
+app.post("/message", (req, res) => {
+  //Respuesta plana
+  // res.send("Mensaje añadido correctamente");
+  //Respuesta con información Status
+  // res.status(201).send({ error: "", body: "Eliminado correctamente" });
+  response.success(req, res, "Creado correctamente", 201);
+});
 app.delete("/message", (req, res) => {
   console.log(req.query);
-  console.log(req.body);
-  res.send("Mensaje " + req.body.text + " eliminado");
+  if (req.query.error == "ok") {
+    response.error(req, res, "Error simulado", 400);
+  } else {
+    console.log(req.body);
+    // res.send("Mensaje " + req.body.text + " eliminado");
+    response.success(req, res, "Eliminado correctamente");
+  }
 });
 
 app.listen(port, () =>
